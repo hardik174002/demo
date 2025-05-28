bee3bf9528cebd4557a6f733ad8ce5d7e6fdf147
Hardik verma <36536977+hardik174002@users.noreply.github.com>
2025-05-28 21:50:20 +0530

    pushing updated jenkins file

---
+++ src/main/java/com/example/demo/FakeController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FakeController {
+
+    @GetMapping("/")
+    public String  hello(){
+        return "Arigato Narayn g!";
+    }
+}
